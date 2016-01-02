e79ac58f58aaf532310ad76cf61f4638d0c28696
steek79 <79steek@gmail.com>
2016-01-02 20:56:04 +0900

    Inheritance

--- partyanimal.py
+++ partyanimal.py
@@ -12,11 +12,18 @@ class PartyAnimal:
         print self.name, 'party count', self.x
 
 
+class FootballFan(PartyAnimal): # subclass inheriting everything from P.A.
+    points = 0
+
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print self.name, 'points', self.points
+
+
 s = PartyAnimal('Sally')
 s.party()
 
-j = PartyAnimal('jim')
+j = FootballFan('Jim')
 j.party()
-s.party()
-
-# s and j are independent instances
+j.touchdown()
