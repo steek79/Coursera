ee6fdc3c73f031e3b4296043b4a4b6260ef7e964
steek79 <79steek@gmail.com>
2016-01-02 18:05:23 +0900

    constructor and destructor

--- partyanimal.py
+++ partyanimal.py
@@ -1,14 +1,18 @@
 class PartyAnimal:
     x = 0
 
+    def __init__(self): # creation (which is common, used to set up variables)
+        print 'I am constructed'
+
     def party(self):
         self.x = self.x + 1
         print 'So far', self.x
 
+    def __del__(self): # destruction (which is rare)
+        print 'I am destructed', self.x
+
 
 an = PartyAnimal()
-
-print dir(an)
 
 an.party()
 an.party()
